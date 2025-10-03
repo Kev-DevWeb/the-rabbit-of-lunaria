60b6c821fc50fb3afc0436b7072a80ba04e4d86c
Kevin García <vinke-ggz@hotmail.com>
2025-10-03 15:58:05 -0600

    fix a todos los bugs

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,11 +1,22 @@
 import { MetadataRoute } from 'next'
 import { client } from '@/sanity/lib/client'
 
+interface Article {
+  slug: string
+  publishedAt: string
+  _updatedAt: string
+}
+
+interface Author {
+  slug: string
+  _updatedAt: string
+}
+
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
   const baseUrl = 'https://www.themadrigueradelunaria.com'
   
   // Obtener todos los artículos de Sanity
-  const articles = await client.fetch(`
+  const articles = await client.fetch<Article[]>(`
     *[_type == "post" && !(_id in path("drafts.**"))] {
       "slug": slug.current,
       publishedAt,
@@ -14,7 +25,7 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
   `)
 
   // Obtener todos los autores
-  const authors = await client.fetch(`
+  const authors = await client.fetch<Author[]>(`
     *[_type == "author"] {
       "slug": slug.current,
       _updatedAt
@@ -50,7 +61,7 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
   ]
 
   // URLs dinámicas de artículos
-  const articlePages: MetadataRoute.Sitemap = articles.map((article: any) => ({
+  const articlePages: MetadataRoute.Sitemap = articles.map((article: Article) => ({
     url: `${baseUrl}/articulos/${article.slug}`,
     lastModified: new Date(article._updatedAt || article.publishedAt),
     changeFrequency: 'weekly' as const,
@@ -58,7 +69,7 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
   }))
 
   // URLs dinámicas de autores
-  const authorPages: MetadataRoute.Sitemap = authors.map((author: any) => ({
+  const authorPages: MetadataRoute.Sitemap = authors.map((author: Author) => ({
     url: `${baseUrl}/autores/${author.slug}`,
     lastModified: new Date(author._updatedAt),
     changeFrequency: 'monthly' as const,
